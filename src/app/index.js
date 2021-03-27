9f18e2a05fd55d5c0a902c093c90dc38520c8943
Brandon Reed <bcreedesign@gmail.com>
2021-03-26 23:45:13 -0400

    built out the app folder structure

---
+++ src/app/index.js
@@ -0,0 +1,3 @@
+import './css/main.scss';
+import './js/main.js';
+const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/app/index.js",
     plugins: [
       new HtmlWebpackPlugin({
         filename: 'index.html',
