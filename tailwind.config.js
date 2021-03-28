f16e1f59974a0e16acd7f8961e5db044799c8e0f
Brandon Reed <bcreedesign@gmail.com>
2021-03-27 22:20:00 -0400

    loading custom fonts, image, and colors into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,29 @@ module.exports = {
   purge: ['./dist/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      backgroundImage: {
+       'hero1': "url('../../img/webhero.jpg')",
+       'photo1': "url('../../img/photos/photo1.jpg')",
+       'photo2': "url('../../img/photos/photo2.jpg')",
+       'photo3': "url('../../img/photos/photo3.jpg')",
+       'photo4': "url('../../img/photos/photo4.jpg')",
+       'photo5': "url('../../img/photos/photo5.jpg')",
+       'photo6': "url('../../img/photos/photo6.jpg')",
+      },
+      backgroundOpacity: ['active'],
+      colors: {
+        blue: '#3a74a6',
+        orange: '#e98324',
+      },
+      fontFamily: {
+        'display': ["Glegoo", 'serif'],
+        'body': ["Maven Pro", 'sans-serif']
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
 }
